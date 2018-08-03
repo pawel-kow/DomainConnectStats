d7a024e2d0c6d7bf9ea54ac904340022e853c0c3
pawel-kow <pawel-kow@users.noreply.github.com>
2018-08-03 10:24:16 +0000

    Prod config

--- test_scan_zonefile.py
+++ test_scan_zonefile.py
@@ -7,17 +7,17 @@ class TestScan_zonefile(TestCase):
         try:
             th2 = 500
             start2 = time.time()
-            cnt2 = scan_zonefile(num_threads=th2, zone_file='com.zone.43656', num_skip=1000)
+            cnt2 = scan_zonefile(num_threads=th2, zone_file='com.zone.43656')
             end2 = time.time()
         finally:
             print_api_providers()
-            dump_api_providers('dump.pckl')
+            dump_api_providers('dump_full.pckl')
 
         print("*****")
         print("{}: {}, {}, {}/domain".format(th2, end2 - start2, cnt2, float(end2 - start2) / float(cnt2)))
         print("*****")
 
     def test_load_api_providers(self):
-        load_api_providers('dump.pckl')
+        load_api_providers('dump_full.pckl')
         print_api_providers()
 
