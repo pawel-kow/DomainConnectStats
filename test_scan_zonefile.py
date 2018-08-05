1882a78e704c36ad5c6ab3a0eec1fa3b0e61bcc7
pawel-kow <pawel-kow@users.noreply.github.com>
2018-08-05 12:29:09 +0000

    Full scan setup

--- test_scan_zonefile.py
+++ test_scan_zonefile.py
@@ -5,19 +5,19 @@ from dc_scanner_test import *
 class TestScan_zonefile(TestCase):
     def test_scan_zonefile(self):
         try:
-            th2 = 20
+            th2 = 250
             start2 = time.time()
-            cnt2 = scan_zonefile(num_threads=th2, zone_file='com.zone.43656', max_domains=10000, skip_first=20000)
+            cnt2 = scan_zonefile(num_threads=th2, zone_file='com.zone.43656')
             end2 = time.time()
         finally:
             print_api_providers()
-            dump_api_providers('dump_test_200_thread.pckl')
+            dump_api_providers('dump_full2.pckl')
 
         print("*****")
         print("{}: {}, {}, {}/domain".format(th2, end2 - start2, cnt2, float(end2 - start2) / float(cnt2)))
         print("*****")
 
     def test_load_api_providers(self):
-        load_api_providers('dump_test_200_thread.pckl')
+        load_api_providers('dump_full2.pckl')
         print_api_providers()
 
